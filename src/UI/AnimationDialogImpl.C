112b393cd74868ea127deb47d831ddcb6a0665be
Lene Preuss <lene.preuss@gmail.com>
2008-03-18 00:17:49 +0000

    added an animation dialog which lets select several settings about an animation written to disk

---
+++ src/UI/AnimationDialogImpl.C
@@ -0,0 +1,34 @@
+
+//      project:      hyperspace explorer
+//      module:
+//      contains:
+//      compile with: make all
+//	author:	      helge preuss (scout@hyperspace-travel.de)
+//	license:      GPL (see License.txt)
+
+#include <iostream>
+
+#include <QFileDialog>
+#include <QString>
+
+#include "AnimationDialogImpl.H"
+
+/** AnimationDialogImpl c'tor taking parameters for the parent AnimationDialog,
+ *  which in turn inherited them from QDialog
+ *  displays the dialog
+ *  @param parent parent widget (NULL)
+ *  @param f window flags                                                     */
+AnimationDialogImpl::AnimationDialogImpl (QWidget *parent, Qt::WFlags f) :
+	QDialog (parent, f) {
+  setupUi(this);
+  connect (fileDialogButton, SIGNAL(clicked()), this, SLOT(openFileDialog()));
+  show ();
+}
+
+void AnimationDialogImpl::openFileDialog() {
+    QString dirName = QFileDialog::getExistingDirectory(
+        this,
+        QObject::tr("Select a directory to save the animation frames"),
+        QDir::currentPath(), QFileDialog::ShowDirsOnly);
+    if (!dirName.isEmpty()) directoryEdit->setText(dirName);
+}
