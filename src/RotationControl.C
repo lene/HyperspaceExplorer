bc7a40c1df6d1bc2afeb8139391e1bf14b64903e
Lene Preuss <lene.preuss@gmail.com>
2009-03-27 23:15:07 +0000

    a widget to enter N-dimensional rotations - only a skeleton now

---
+++ src/RotationControl.C
@@ -0,0 +1,20 @@
+///
+/// C++ Implementation: ColorManager
+///
+/// Description:
+///
+///
+/// \author Helge Preuss <scout@hyperspace-travel.de>, (C) 2008
+///
+/// Copyright: See COPYING file that comes with this distribution
+///
+///
+#include "RotationControl.H"
+
+namespace UI {
+
+    RotationControl::RotationControl(VecMath::RotationBase r, QWidget *parent):
+            QWidget(parent) {
+    }
+
+}
---
+++ src/RotationSlider.C
@@ -0,0 +1,41 @@
+///
+/// C++ Implementation: ColorManager
+///
+/// Description:
+///
+///
+/// \author Helge Preuss <scout@hyperspace-travel.de>, (C) 2008
+///
+/// Copyright: See COPYING file that comes with this distribution
+///
+///
+
+#include "RotationSlider.H"
+
+#include <cmath>
+
+namespace UI {
+
+    RotationSlider::RotationSlider(unsigned precision, QWidget *parent):
+            QSlider(parent), _precision(1.) {
+        while(precision--) _precision *= 10;
+        setMaximum(180*_precision);
+        setMinimum(-180*_precision);
+        //  change value by 1 degree when pressing an arrow key
+        setSingleStep(_precision);
+        //  change value by 10 degrees when pressing PgUp/PgDn
+        setPageStep(10*_precision);
+
+        setTickPosition(QSlider::TicksBelow);
+        setTickInterval(10*_precision);
+    }
+
+    double RotationSlider::value() const {
+        return (double)QSlider::value()/180./_precision*M_PI;
+    }
+
+    void RotationSlider::setValue(double rot) {
+        QSlider::setValue(rot/M_PI*180.*_precision);
+    }
+
+}
