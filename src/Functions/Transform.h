f526db271602f4175375ae606fc9ea308a28c3ee
Lene Preuss <lene.preuss@gmail.com>
2009-11-26 23:27:37 +0000

    Dimension of the transformationb matrix and the vector are, of course, identical.

--- src/Functions/Transform.h
+++ src/Functions/Transform.h
@@ -27,73 +27,138 @@
 
 #include <QtConcurrentMap>
 
+/// Transforms Vertices or collections thereof according to a Rotation and a translation Vector.
+/** The rotation Matrix and translation Vector are stored as class members, thus
+ *  the name copy_member_transform.
+ *
+ *  operator() is called recursively on collections (works only for std::vector
+ *  now) until the transformation has been called on every element.
+ *
+ * \param Type Class the transform is performed upon. Must be either 
+ *    VecMath::Vector, or a std::vector of objects (which in turn can be either
+ *    Vector or another, potentially recursive, vector).
+ * \param N Dimension of the vector space in which the transform takes place.
+ */
 template <typename Type, unsigned N> class copy_member_transform {
   public: 
     
+    /// Initializes the transform.
+    /** \param Rot The rotation Matrix to be executed.
+     *  \param T The Translation to be applied.
+     *  \param X The object (vertex or collection of vertices) to be transformed.
+     */
     copy_member_transform(VecMath::Matrix<N> &Rot, const VecMath::Vector<N> &T, const Type &X):  
-	m_Rot(Rot), m_T(T), m_X(X) { }
+      m_Rot(Rot), m_T(T), m_X(X) { }
     
+    /// Executes the transform. 
+    /** This is not defined for the generic case, but must be specialized for 
+     *  vertices and collections of vertices separately.
+     */
     void operator() (Type &Xtrans);
 
   private:
-    VecMath::Matrix<N> &m_Rot;
-    const VecMath::Vector<N> &m_T;
-    const Type &m_X;
+    VecMath::Matrix<N> &m_Rot;      ///< Rotation to be executed.
+    const VecMath::Vector<N> &m_T;  ///< Translation to be executed.
+    const Type &m_X;                ///< Object to be transformed.
 };
 
+/// Specialization for a transform on a std::vector of objects 
+/** \param Type Class the transform is performed upon. Must be either 
+ *    VecMath::Vector, or a std::vector of objects (which in turn can be either
+ *    Vector or another, potentially recursive, vector).
+ *  \param N Dimension of the vector space in which the transform takes place.
+ */
 template <typename Type, unsigned N> class copy_member_transform< std::vector<Type>, N > {
   public: 
 
+    /// Initializes the transform.
+    /** \param Rot The rotation Matrix to be executed.
+     *  \param T The Translation to be applied.
+     *  \param X The collection of vertices to be transformed.
+     */
     copy_member_transform(VecMath::Matrix<N> &Rot, const VecMath::Vector<N> &T, const std::vector<Type> &X):  
-	m_Rot(Rot), m_T(T), m_X(X) { }
+      m_Rot(Rot), m_T(T), m_X(X) { }
 
+    /// Static convenience function easing the initialization of the transformation.
+    /** \param Rot The rotation Matrix to be executed.
+     *  \param T The Translation to be applied.
+     *  \param X The collection of vertices to be transformed.
+     *  \param Xtrans The transformed vertices.
+     */
     static void xform(VecMath::Matrix<N> &Rot,
-		      const VecMath::Vector<N> &T,
-		      const std::vector<Type> &X, std::vector<Type> &Xtrans) {
-    	/* static */ copy_member_transform<std::vector<Type>, N> trans(Rot, T, X);
-    	trans(Xtrans);
+      const VecMath::Vector<N> &T,
+      const std::vector<Type> &X, std::vector<Type> &Xtrans) {
+      copy_member_transform<std::vector<Type>, N> trans(Rot, T, X);
+      trans(Xtrans);
     }
 
+    /// Executes the transform for every vertex in the collection.
+    /** For each member of the collection \p m_X, a copy_member_transform is 
+     *  recursively created and executed.
+     *  \param Xtrans The collection holding the transformed vertices.
+     */
     void operator() (std::vector<Type> &Xtrans) {
-    	typename std::vector<Type>::const_iterator i = m_X.begin();
-    	typename std::vector<Type>::iterator j = Xtrans.begin();
-    	while(i != m_X.end() && j != Xtrans.end()) {
-    		copy_member_transform<Type, N>::xform(m_Rot, m_T, *i, *j);
-    		++i; ++j;
-    	}
+      typename std::vector<Type>::const_iterator i = m_X.begin();
+      typename std::vector<Type>::iterator j = Xtrans.begin();
+      while(i != m_X.end() && j != Xtrans.end()) {
+        copy_member_transform<Type, N>::xform(m_Rot, m_T, *i, *j);
+        ++i; ++j;
+      }
       // QtConcurrent::map(X, copy_member_transform<Type, N>::xform);
       }
 
   private:
-    VecMath::Matrix<N> &m_Rot;
-    const VecMath::Vector<N> &m_T;
-    const std::vector<Type> &m_X;
+    VecMath::Matrix<N> &m_Rot;      ///< Rotation to be executed.
+    const VecMath::Vector<N> &m_T;  ///< Translation to be executed.
+    const std::vector<Type> &m_X;   ///< Collection of objects to be transformed.
 };
 
-template <unsigned D, unsigned N> class copy_member_transform< VecMath::Vector<D>, N > {
+/// Specialization for a transform on a single VecMath::Vector
+/** \param N Dimension of the vector space in which the transform takes place.
+ */
+template <unsigned N> class copy_member_transform< VecMath::Vector<N>, N > {
   public: 
 
-    copy_member_transform(VecMath::Matrix<N> &Rot, const VecMath::Vector<N> &T, const VecMath::Vector<D> &X):  
-	m_Rot(Rot), m_T(T), m_X(X) { }
+    /// Initializes the transform.
+    /** \param Rot The rotation Matrix to be executed.
+     *  \param T The Translation to be applied.
+     *  \param X The collection of vertices to be transformed.
+     */
+    copy_member_transform(VecMath::Matrix<N> &Rot, const VecMath::Vector<N> &T, const VecMath::Vector<N> &X):  
+      m_Rot(Rot), m_T(T), m_X(X) { }
 
+    /// Static convenience function easing the initialization of the transformation.
+    /** \param Rot The rotation Matrix to be executed.
+     *  \param T The Translation to be applied.
+     *  \param X The vertex to be transformed.
+     *  \param Xtrans The transformed vertex.
+     */
     static void xform(VecMath::Matrix<N> &Rot, const VecMath::Vector<N> &T,
-		      const VecMath::Vector<D> &X, VecMath::Vector<D> &Xtrans) {
-    	/* static */ copy_member_transform<VecMath::Vector<D>, N> trans(Rot, T, X);
-    	trans(Xtrans);
+      const VecMath::Vector<N> &X, VecMath::Vector<N> &Xtrans) {
+      copy_member_transform<VecMath::Vector<N>, N> trans(Rot, T, X);
+      trans(Xtrans);
     }
 
-    void operator() (VecMath::Vector<D> &Xtrans) {
-//      std::cerr << m_Rot << " * " << m_X << " + " << m_T;
+    /// Executes the transform on the vertex.
+    /** \param Xtrans The transformed vertex.
+     */
+    void operator() (VecMath::Vector<N> &Xtrans) {
       Xtrans = (m_Rot*m_X)+m_T;
-//      std::cerr << Xtrans << std::endl;
     }
 
   private:
-    VecMath::Matrix<N> &m_Rot;
-    const VecMath::Vector<N> &m_T;
-    const VecMath::Vector<D> &m_X;
+    VecMath::Matrix<N> &m_Rot;      ///< Rotation to be executed.
+    const VecMath::Vector<N> &m_T;  ///< Translation to be executed.
+    const VecMath::Vector<N> &m_X;  ///< Vertex to be transformed.
 };
 
+
+/// An alternative transformation class template, taking the arguments as function parameters instead of member variables
+/** \param Type Class the transform is performed upon. Must be either 
+ *    VecMath::Vector, or a std::vector of objects (which in turn can be either
+ *    Vector or another, potentially recursive, vector).
+ * \param N Dimension of the vector space in which the transform takes place.
+ */
 template <typename Type, unsigned N> class call_parameters_transform {
   public:
 
@@ -102,11 +167,18 @@ template <typename Type, unsigned N> class call_parameters_transform {
                      const Type &X, Type &Xtrans);
 };
 
+/// Specialization for a transform on a std::vector of objects 
+/** \param Type Class the transform is performed upon. Must be either 
+ *    VecMath::Vector, or a std::vector of objects (which in turn can be either
+ *    Vector or another, potentially recursive, vector).
+ * \param N Dimension of the vector space in which the transform takes place.
+ */
 template <typename Type, unsigned N> class call_parameters_transform< std::vector<Type>, N > {
   public:
     static void xform(VecMath::Matrix<N> &Rot,
-                                          const VecMath::Vector<N> &T,
-                                          const std::vector<Type> &X, std::vector<Type> &Xtrans) {
+                      const VecMath::Vector<N> &T,
+                      const std::vector<Type> &X, 
+                      std::vector<Type> &Xtrans) {
         static call_parameters_transform<std::vector<Type>, N> trans;
         trans(Rot, T, X, Xtrans);
     }
@@ -115,36 +187,44 @@ template <typename Type, unsigned N> class call_parameters_transform< std::vecto
                      const VecMath::Vector<N> &T,
                      const std::vector<Type> &X, std::vector<Type> &Xtrans) {
 
-
         typename std::vector<Type>::const_iterator i = X.begin();
         typename std::vector<Type>::iterator j = Xtrans.begin();
         while(i != X.end() && j != Xtrans.end()) {
-                call_parameters_transform<Type, N>::xform(Rot, T, *i, *j);
-                ++i; ++j;
+          call_parameters_transform<Type, N>::xform(Rot, T, *i, *j);
+          ++i; ++j;
         }
       }
 
    //   QtConcurrent::map(X, transform<Type, N>::xform);
 };
 
-template <unsigned D, unsigned N> class call_parameters_transform< VecMath::Vector<D>, N > {
+/// Specialization for a transform on a single VecMath::Vector
+/** \param N Dimension of the vector space in which the transform takes place.
+ */
+template <unsigned N> class call_parameters_transform< VecMath::Vector<N>, N > {
   public:
 
     static void xform(VecMath::Matrix<N> &Rot,
                                           const VecMath::Vector<N> &T,
-                                          const VecMath::Vector<D> &X, VecMath::Vector<D> &Xtrans) {
-        static call_parameters_transform<VecMath::Vector<D>, N> trans;
+                                          const VecMath::Vector<N> &X, VecMath::Vector<N> &Xtrans) {
+        static call_parameters_transform<VecMath::Vector<N>, N> trans;
         trans(Rot, T, X, Xtrans);
     }
 
     void operator() (VecMath::Matrix<N> &Rot, const VecMath::Vector<N> &T,
-                     const VecMath::Vector<D> &X, VecMath::Vector<D> &Xtrans) {
+                     const VecMath::Vector<N> &X, VecMath::Vector<N> &Xtrans) {
         Xtrans = (Rot*X)+T;
     }
 };
 
 
-
+/// Default transform class - Okay, this is a hack, because i can't decide yet which transform is better suited
+/** \todo Make the transformation to be used a policy to the Function class.
+ *  \param Type Class the transform is performed upon. Must be either 
+ *    VecMath::Vector, or a std::vector of objects (which in turn can be either
+ *    Vector or another, potentially recursive, vector).
+ *  \param N Dimension of the vector space in which the transform takes place.
+ */
 template <typename Type, unsigned N> class transform: public copy_member_transform<Type, N> {
 };
 
