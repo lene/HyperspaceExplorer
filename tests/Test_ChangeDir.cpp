088fdc81d8f9d316c7560675df8c8c33e553ff65
Lene Preuss <lene.preuss@gmail.com>
2014-04-02 15:59:42 +0200

    added or fixed GPL header where applicable

--- tests/Test_ChangeDir.cpp
+++ tests/Test_ChangeDir.cpp
@@ -1,8 +1,21 @@
-/* 
- * File:   Test_ChangeDir.cpp
- * Author: lene
- * 
- * Created on 28 March 2014, 18:38
+/*
+ * Hyperspace Explorer - visualizing higher-dimensional geometry
+ * Copyright (C) 2011  Lene Preuss <lene.preuss@gmail.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
  */
 
 #include "Test_ChangeDir.h"
