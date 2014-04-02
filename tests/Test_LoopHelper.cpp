920aebfc28b2e39c95d1989497c84fc83764c7ee
Lene Preuss <lene.preuss@gmail.com>
2014-04-02 12:36:10 +0200

    added test skeletons for LoopHelper (#117)

---
+++ tests/Test_LoopHelper.cpp
@@ -0,0 +1,9 @@
+/* 
+ * File:   Test_LoopHelper.cpp
+ * Author: lene
+ * 
+ * Created on April 1, 2014, 10:28 PM
+ */
+
+#include "Test_LoopHelper.h"
+#include "GlobalFunctions.h"
---
+++ tests/Test_LoopHelper.h
@@ -0,0 +1,22 @@
+/* 
+ * File:   Test_LoopHelper.h
+ * Author: lene
+ *
+ * Created on April 1, 2014, 10:28 PM
+ */
+
+#ifndef TEST_LOOPHELPER_H
+#define	TEST_LOOPHELPER_H
+
+#include <QtTest/QtTest>
+
+class Test_LoopHelper: public QObject {
+    
+    Q_OBJECT
+    
+private:
+
+};
+
+#endif	/* TEST_LOOPHELPER_H */
+
